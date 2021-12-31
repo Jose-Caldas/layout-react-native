4759ea4bbe4c15c5875071bf50bd64884106dc91
José Caldas <jrcaldas@yahoo.com.br>
2021-12-31 13:27:56 -0300

    feat: initial structure

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import styles from "./header.styles";
+
+const Header = () => {
+  return (
+    <View>
+      <Text style={styles.container}>Header</Text>
+    </View>
+  );
+};
+
+export default Header;
